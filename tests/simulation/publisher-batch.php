9d8e07ae6df879d5efec445f91ff7dea788e6063
Dmitriy Krivopalov <dimarik82@gmail.com>
2025-08-15 11:07:48 +0300

    Merge pull request #6
    * publisher batch

---
+++ tests/simulation/publisher-batch.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Publisher.
+ * @var Builder $builder bootstrap.php
+ */
+
+declare(strict_types=1);
+
+use kuaukutsu\poc\queue\redis\Builder;
+use kuaukutsu\poc\queue\redis\tests\stub\QueueHandlerStub;
+use kuaukutsu\poc\queue\redis\tests\stub\QueueSchemaStub;
+use kuaukutsu\queue\core\QueueContext;
+use kuaukutsu\queue\core\QueueTask;
+
+use function kuaukutsu\poc\queue\redis\tests\argument;
+
+require dirname(__DIR__) . '/bootstrap.php';
+
+$schema = QueueSchemaStub::from((string)argument('schema', 'low'));
+echo 'publisher run: ' . $schema->getRoutingKey() . PHP_EOL;
+
+$batch = [];
+foreach (range(1, 100) as $item) {
+    $batch[] = new QueueTask(
+        target: QueueHandlerStub::class,
+        arguments: [
+            'id' => $item,
+            'name' => 'test confirm',
+        ],
+    );
+}
+
+$publisher = $builder->buildPublisher();
+$publisher->pushBatch(
+    $schema,
+    $batch,
+    QueueContext::make($schema)->withExternal(['requestId' => $item])
+);
