f1cc4b1eafa53118a56dd5dc1e72eaa55c22bf95
Dmitriy Krivopalov <dimarik82@gmail.com>
2025-08-15 08:40:14 +0300

    Merge pull request #4
    * update dependencies
    * timeout blocking
    * remove artifact
    * add rule rector

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -16,6 +17,12 @@ return static function (RectorConfig $rectorConfig): void {
 
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->ruleWithConfiguration(
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+        [
+            'allow_override_empty_method' => true,
+        ]
+    );
 
     // define sets of rules
     $rectorConfig->sets([
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 use DI\Container;
 use Amp\Redis\RedisConfig;
 use kuaukutsu\poc\queue\redis\Builder;
+use kuaukutsu\poc\queue\redis\internal\FactoryProxy;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $container = new Container();
-$builder = (new Builder($container))
+$builder = (new Builder(new FactoryProxy($container)))
     ->withConfig(
         RedisConfig::fromUri('redis://redis:6379')
     );
