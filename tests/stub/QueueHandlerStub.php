5f6becd48b98fbec8abf7cf2bb9aec0313f0933d
Dmitriy Krivopalov <dimarik82@gmail.com>
2025-08-17 11:27:48 +0300

    fix simulation

--- tests/stub/QueueHandlerStub.php
+++ tests/stub/QueueHandlerStub.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace kuaukutsu\poc\queue\redis\tests\stub;
 
 use Override;
-use Revolt\EventLoop;
 use kuaukutsu\queue\core\QueueContext;
 use kuaukutsu\queue\core\TaskInterface;
 
@@ -21,13 +20,6 @@ final readonly class QueueHandlerStub implements TaskInterface
     #[Override]
     public function handle(QueueContext $context): void
     {
-        $id = $this->id;
-        $name = $this->name;
-        $writer = $this->writer;
-        $delay = ($id % 5) === 0 ? 2. : 0.5;
-        // check  non-blocking
-        EventLoop::delay($delay, static function () use ($id, $name, $context, $writer): void {
-            $writer->print($id, $name, $context);
-        });
+        $this->writer->print($this->id, $this->name, $context);
     }
 }
